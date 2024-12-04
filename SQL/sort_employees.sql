2a933bf6ee55b042ecf3b5d6be007bb7a0447e3d
MashaBurger <tyunina.maria.tsar@gmail.com>
2024-12-04 17:08:33 +0700

    Update sort_employees.sql

--- SQL/sort_employees.sql
+++ SQL/sort_employees.sql
@@ -2,6 +2,7 @@
 
 create database employees;
 
+/*Условие: Имеется таблица Сотрудники (sotr) со следующей структурой: */
 create table employees.sotr (
     name varchar(100),
     address varchar(100),
@@ -18,5 +19,27 @@ insert into employees.sotr values
 
 
 /*Необходимо написать запрос, который выводит все записи по сотрудникам, родившимся в 1977 - 1983 годах*/
-
 select * from employees.sotr where birthdate between '1977-01-01' and '1983-12-31';
+
+/*выводим содержимое таблицы sotr по сотрудникам, у которых пол – женский */
+select * from employees.sotr where sex='W';
+
+/*Условие: Есть еще одна таблица zarp со следующей структурой: */
+create table employees.zarp (
+	sotr_name varchar(100),
+    sotr_zarp decimal(6,2)
+);
+
+insert into employees.zarp values
+('Петр', '100'),
+('Федор', '200'),
+('Маша', '300');
+
+/* из таблиц SOTR и ZARP выводим информацию по сотруднику Федор:
+Имя сотрудника; Адрес; Зарплата */
+select name, address, sotr_zarp from employees.sotr join employees.zarp on zarp.sotr_name = sotr.name where name='Федор';
+
+
+
+
+
